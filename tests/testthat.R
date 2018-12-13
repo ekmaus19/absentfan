e4bccf7b86fd25ecb04c290a85b763626b0170b6
Elizaveta Kravchenko <ekravchenko@wesleyan.edu>
2018-12-13 11:17:10 -0500

    final tweaks

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,21 +3,4 @@ library(absentfan)
 
 test_check("absentfan")
 
-test_that("getUrl Works",{
-  expect_equal(getUrl("Harry Potter", "book"),list(url="https://www.fanfiction.net/book/Harry-Potter/",max.entries=NA))
-  expect_equal(getUrl("Zoolander", "movie", max.entries = 2),list(url="https://www.fanfiction.net/movie/Zoolander/",max.entries=2))
-})
-
-test_that("getPages for entries works",{
-  expect_equal(getPages(list(url="https://www.fanfiction.net/book/Harry-Potter/",max.entries=NA)),
-               list(noPages=FALSE, page_use="/book/Harry-Potter/?&srt=1&r=103&p=", page_num=11, url="https://www.fanfiction.net/book/Harry-Potter/", max.entries=NA))
-  expect_equal(getPages(list(url="https://www.fanfiction.net/movie/Zoolander/",max.entries=2)),
-               list(noPages=TRUE, page_use=NA, page_num=NA, url="https://www.fanfiction.net/movie/Zoolander/", max.entries=2))
-})
-
-test_that("all_entries returns a dataframe", {
-  expect_equal(typeof(all_entries(getEntries(getPages(getUrl("Zoolander", "movie"))))), "list")
-})
-
-
 
